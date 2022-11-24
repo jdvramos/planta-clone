f4f07f93413fe1148a41f7ce353906c3338776c0
jdvramos <jonndenzelramos@gmail.com>
2022-11-24 19:56:13 +0800

    reviews section done

---
+++ js/script.js
@@ -0,0 +1,39 @@
+// var swiper = new Swiper(".slide-content ", {
+//   slidesPerView: 1,
+//   spaceBetween: 30,
+//   //   slidesPerGroup: 3,
+//   loop: true,
+//   loopFillGroupWithBlank: true,
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+// });
+
+var swiper = new Swiper(".slide-content", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  initialSlide: 3,
+  loop: true,
+  grabCursor: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+  // this was actually disabled
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // optional
+  autoplay: {
+    delay: 5000,
+    pauseOnMouseEnter: true,
+  },
+});
