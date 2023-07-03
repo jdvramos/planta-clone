6442e695560b5f0f7e6505c0be5aa6455582bcae
jdvramos <jonndenzelramos@gmail.com>
2023-07-03 13:30:43 +0800

    project finished

--- js/script.js
+++ js/script.js
@@ -1,19 +1,3 @@
-// var swiper = new Swiper(".slide-content ", {
-//   slidesPerView: 1,
-//   spaceBetween: 30,
-//   //   slidesPerGroup: 3,
-//   loop: true,
-//   loopFillGroupWithBlank: true,
-//   pagination: {
-//     el: ".swiper-pagination",
-//     clickable: true,
-//   },
-//   navigation: {
-//     nextEl: ".swiper-button-next",
-//     prevEl: ".swiper-button-prev",
-//   },
-// });
-
 var swiper = new Swiper(".slide-content", {
   slidesPerView: 1,
   spaceBetween: 30,
